5355973d8acf17ea1ab62004fe882e2d066fd6b3
HJtime <72139168+HJtime@users.noreply.github.com>
2021-01-07 14:28:30 +0900

    JS main Slide

--- main.js
+++ main.js
@@ -7,3 +7,56 @@ const navbarToggle=document.querySelector('.navbar__toggle-btn');
 navbarToggle.addEventListener('click',()=>{
     navbarMenu.classList.toggle('active');
 });
+
+// 메인 슬라이드
+const slides = document.querySelectorAll('.main__slides');
+const dots =  document.querySelectorAll('.dots');
+let slideIndex = 0;
+
+let myVar;
+
+showSlides();
+
+function showSlides(){
+    for(let i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+        dots[i].setAttribute('class', '');
+    }
+    
+    slideIndex++;
+
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+    }
+
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].setAttribute('class', 'active');
+
+    myVar = setTimeout(showSlides, 2000);
+}//showSlides
+
+function stopSlide(){
+    clearTimeout(myVar);
+} //stopSlide
+
+const dotsBox=document.querySelector('.dotContainer');
+
+dotsBox.addEventListener('click', (event)=>{
+    const filter=event.target.dataset.dot || event.target.parentNode.dataset.dot;
+
+    currentSlide(filter);
+})
+
+/* ========= */
+function currentSlide(num){
+    stopSlide();
+    slideIndex = num;
+
+    for(let i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+        dots[i].setAttribute('class', '');
+    }
+    
+    slides[slideIndex].style.display = 'block';
+    dots[slideIndex].setAttribute('class', 'active');
+}
